064bfa44f3c9eac025089fef30711960d0aee259
bkshazzad <shazzadjihad@gmail.com>
2021-07-04 17:21:05 +0600

    zakkash

---
+++ js/apps.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.banner').slick({
+        prevArrow:'<i class="fas fa-angle-right "></i>',
+        nextArrow:'<i class="fas fa-angle-left"></i>',
+    //   autoplay: true,
+    //   autoplaySpeed:1000,
+      dots: true,
+      dotsClass: 'slick-dots',
+    });
+})
